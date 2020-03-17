0ce0b6aa1ba5c7671de8e61f7baf88c03fccc087
Liam Cooke <noreply@liamcooke.com>
2020-03-17 19:47:14 +1100

    conversions/try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::try_from("Mark,20")` to compile
 // and return an Ok result of inner type Person.
@@ -28,6 +27,16 @@ struct Person {
 impl TryFrom<&str> for Person {
     type Error = String;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let values: Vec<&str> = s.split(',').collect();
+        if values.len() == 2 {
+            let name = values[0].to_string();
+            match values[1].parse::<usize>() {
+                Ok(age) => Ok(Person { name, age }),
+                Err(_) => Err("could not parse age".into()),
+            }
+        } else {
+            Err("expected 2 values".into())
+        }
     }
 }
 
